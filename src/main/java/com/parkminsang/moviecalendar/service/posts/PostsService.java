9cb60567fdb74aa049c05be89cc768cb332d52c3
MinsangPark <whsrpdla@naver.com>
2021-10-10 20:33:59 +0900

    posts 등록, 수정 기능 추가 & 테스트 코드 추가

---
+++ src/main/java/com/parkminsang/moviecalendar/service/posts/PostsService.java
@@ -0,0 +1,39 @@
+package com.parkminsang.moviecalendar.service.posts;
+
+import com.parkminsang.moviecalendar.domain.posts.Posts;
+import com.parkminsang.moviecalendar.domain.posts.PostsRepository;
+import com.parkminsang.moviecalendar.web.dto.PostsResponseDto;
+import com.parkminsang.moviecalendar.web.dto.PostsSaveRequestDto;
+import com.parkminsang.moviecalendar.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id= "+id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id= "+id));
+
+        return new PostsResponseDto(entity);
+    }
+
+}
---
+++ src/main/java/com/parkminsang/moviecalendar/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.parkminsang.moviecalendar.web;
+
+import com.parkminsang.moviecalendar.service.posts.PostsService;
+import com.parkminsang.moviecalendar.web.dto.PostsResponseDto;
+import com.parkminsang.moviecalendar.web.dto.PostsSaveRequestDto;
+import com.parkminsang.moviecalendar.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
