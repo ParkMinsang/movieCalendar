6af6be9881e430ef3c0aba259486f24dc3ed5718
MinsangPark <whsrpdla@naver.com>
2021-10-11 21:05:27 +0900

    게시글 수정,삭제 기능 추가 & 테스트 코드 추가

--- src/test/java/com/parkminsang/moviecalendar/web/PostsApiControllerTest.java
+++ src/test/java/com/parkminsang/moviecalendar/web/PostsApiControllerTest.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -95,4 +92,30 @@ public class PostsApiControllerTest {
         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
         assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
+
+    @Test
+    public void Posts_삭제된다() throws Exception{
+        //given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        Long deleteId = savedPosts.getId();
+
+        String url = "http://localhost:"+port+"/api/v1/posts/"+deleteId;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        HttpEntity<String> requestEntity = new HttpEntity<>(httpHeaders);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.DELETE, requestEntity, Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.size()).isEqualTo(0);
+    }
+
 }
